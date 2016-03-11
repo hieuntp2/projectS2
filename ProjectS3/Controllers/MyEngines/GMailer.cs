96bb82811f1a9eb0e07c128e22ae795e893a08b9
hieuntp2 <hieuntp2@gmail.com>
2016-03-11 15:18:46 +0700

    d

--- ProjectS3/Controllers/MyEngines/GMailer.cs
+++ ProjectS3/Controllers/MyEngines/GMailer.cs
@@ -45,7 +45,7 @@ namespace ProjectS3.Controllers.MyEngines
         private static string Base64UrlEncode(string input)
         {
             var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
-            //Special "url-safe" base64 encode.
+             //Special "url-safe" base64 encode.
             return Convert.ToBase64String(inputBytes)
               .Replace('+', '-')
               .Replace('/', '_')
