e963a343c96ca0ea9763d0a337b479561edec714
hieuntp2 <hieuntp2@gmail.com>
2016-03-11 16:33:46 +0700

    13

--- ProjectS3/Controllers/MyEngines/GMailer.cs
+++ ProjectS3/Controllers/MyEngines/GMailer.cs
@@ -5,6 +5,7 @@ using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using System.Runtime.Remoting.Contexts;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace ProjectS3.Controllers.MyEngines
 {
@@ -56,6 +57,38 @@ namespace ProjectS3.Controllers.MyEngines
         {
         }
 
+        //public void Send(string subject, string messagebody)
+        //{
+        //    MyDynamicValues mydynamic = new MyDynamicValues();
+        //    string mytoemail = mydynamic.getValue("toemail");
+        //    string myfromemail = mydynamic.getValue("fromemail");
+        //    string mypassword = mydynamic.getValue("password");
+
+        //    var fromAddress = new MailAddress(myfromemail, "Inthef.vn");
+        //    var toAddress = new MailAddress(mytoemail, "Website Inthef.vn");
+        //    string fromPassword = mypassword;
+        //    string mysj = "Inthef.vn: " + subject;
+        //    string body = messagebody;
+
+        //    var smtp = new SmtpClient
+        //    {
+        //        Host = "smtp.gmail.com",
+        //        Port = 587,
+        //        EnableSsl = true,
+        //        DeliveryMethod = SmtpDeliveryMethod.Network,
+        //        UseDefaultCredentials = true,
+        //        Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+        //    };
+        //    using (var message = new MailMessage(fromAddress, toAddress)
+        //    {
+        //        Subject = mysj,
+        //        Body = body
+        //    })
+        //    {
+        //        smtp.Send(message);
+        //    }
+        //}
+
         public void Send(string subject, string messagebody)
         {
             MyDynamicValues mydynamic = new MyDynamicValues();
@@ -69,21 +102,24 @@ namespace ProjectS3.Controllers.MyEngines
             string mysj = "Inthef.vn: " + subject;
             string body = messagebody;
 
-            var smtp = new SmtpClient
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(mytoemail));  // replace with valid value 
+            message.From = new MailAddress(myfromemail);  // replace with valid value
+            message.Subject = "Your email subject";
+            message.Body = string.Format(body, "From name: HIEU", "From email: " + myfromemail, "This is message");
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
             {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = true,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-            };
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = mysj,
-                Body = body
-            })
-            {
+                var credential = new NetworkCredential
+                {
+                    UserName = myfromemail,  // replace with valid value
+                    Password = mypassword  // replace with valid value
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
                 smtp.Send(message);
             }
         }
